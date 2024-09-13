48f75c50d9d9eb368d4fd4fef303d59c8a007db2
Pukyx <151487540+Pukyx@users.noreply.github.com>
2024-09-13 12:20:49 -0300

    parte media

--- proyectoEva1/appPeliculas/views.py
+++ proyectoEva1/appPeliculas/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def render(request): 
+    return request  
+
+def peli1(request):
+    data = {'nombre': 'Barbie y el Secreto de las Hadas','Descripción':'', 'valoración': 4} 
+    return request 
+
+def peli2(request):
+    data = {'nombre': 'Barbie Fairytopia','Descripción':'', 'valoración': 5} 
+    return request
+
+def peli3(request):
+    data = {'nombre': 'Barbie y las 12 princesas bailarinas','Descripción':'', 'valoración': 0} 
+    return request
